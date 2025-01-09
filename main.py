f1a18d9894d3387b5334de801ed1424ce0d3ab7e
Diego Serpelloni <diegoserpelloni03@gmail.com>
2025-01-09 15:08:49 +0100

    implementazione funzioni base

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-#lavoriamo a dizionario
-
-
 def creazioneUtente():
     nome = input("Inserisci il tuo nome: ")
     cognome = input("Inserisci il tuo cognome: ")
     utente = {"nome": nome, "cognome": cognome}
     return utente
 
-def eliminaUtente():
-    return
+def eliminaUtente(listaUtenti):
+    nome_Utente = input("Inserisci il nome dell'utente da eliminare: ")
+    cognome_Utente = input("Inserisci il cognome dell'utente da eliminare: ")
+    for x in range(len(listaUtenti) - 1, -1, -1):
+        if listaLibri[x]['nome'] == nome_Utente and listaLibri[x]['cognome'] == cognome_Utente:
+            del listaLibri[x]
+    return 0
 
-def cercaUtente():
+def cercaUtente(listaUtenti):
+    nome_Utente = input("Inserisci il nome della persona da cercare: ")
+    cognome_Utente = input("Inserisci il cognome della persona da cercare: ")
+    for x in range(len(listaUtenti) - 1, -1, -1):
+        if listaUtenti[x]['nome'] == nome_Utente and listaUtenti[x]['cognome'] == cognome_Utente:
+            print(listaUtenti[x])
     return
 
 def aggiungiLibro():
@@ -21,10 +28,17 @@ def aggiungiLibro():
     return libro
 
 def eliminaLibro(listaLibri):
-    del listaLibri['nome' == input("Inserisci il nome del libro da eliminare: ")]
+    nome_libro = input("Inserisci il nome del libro da eliminare: ")
+    for x in range(len(listaLibri) - 1, -1, -1):
+        if listaLibri[x]['nome'] == nome_libro:
+            del listaLibri[x]
     return 0
 
-def ricercaLibro():
+def ricercaLibro(listaLibri):
+    nome_libro = input("Inserisci il nome del libro da cercare: ")
+    for x in range(len(listaLibri) - 1, -1, -1):
+        if listaLibri[x]['nome'] == nome_libro:
+            print(listaLibri[x])
     return
 
 def prestitoLibro():
@@ -33,8 +47,6 @@ def prestitoLibro():
 def restituzioneLibro():
     return
 
-def gestioneLibri():
-    return
 
 
 
@@ -43,7 +55,7 @@ def gestioneLibri():
 listaLibri = []
 listaUtenti = []
 while(True):
-    x = int(input("1) Aggiungi Libri 2) Aggiungi Utenti 3) Elimina Libri 4) Elimina Utenti 97) catalogo libri 98) lista utenti 99) exit "))
+    x = int(input("1) Aggiungi Libri 2) Aggiungi Utenti 3) Elimina Libri 4) Elimina Utenti 5) Ricerca Libri 6) Cerca Utenti  97) catalogo libri 98) lista utenti 99) exit "))
     match x:
         case 1:
             listaLibri.append(aggiungiLibro())
@@ -52,7 +64,11 @@ while(True):
         case 3:
             eliminaLibro(listaLibri)
         case 4:
-            eliminaUtente()
+            eliminaUtente(listaUtenti)
+        case 5:
+            ricercaLibro(listaLibri)
+        case 6:
+            cercaUtente(listaUtenti)
         case 97:
             print(listaLibri)
         case 98:
